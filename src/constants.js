660bc3d021ae551476d1ea0b708d66fef15cda96
Lyman Shen <lyman.shen@gmail.com>
2021-01-22 10:31:09 -0800

    Use a working Base API

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://kur4j57ved.execute-api.us-west-1.amazonaws.com/dev/api/v2/';
+export const BASE_URL = 'https://ht56vci4v9.execute-api.us-west-1.amazonaws.com/dev/api/v2/';
